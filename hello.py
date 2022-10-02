688a9f0456c035c5b8c10f0b92fe3b9310d144c7
AhnTaejin <taejin22@dongguk.edu>
2022-10-02 17:04:18 +0900

    2022-10-22 실습과제#5 7번

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ name = input("이름을 입력하세요: ")
 number = input("학번을 입력하세요: ")
 department = input("학과를 입력하세요: ")
 school = input("학교를 입력하세요: ")
+grade = input("학년을 입력하세요: ")
 
 print("\n<출력>")
-print(f'이름: {name}\n학번: {number}\n학과: {department}\n학교: {school}')
+print(f'이름: {name}\n학번: {number}\n학과: {department}\n학교: {school}\n학년: {grade}')
